5ef3a96e9234744b17952d04415cc7c25efe637d
Carolene Wilson <carolenesw@gmail.com>
2020-09-21 10:11:13 -0400

    render location coordinates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import ListPerson from './components/list-person.component';
-import EditPerson from './components/edit-person.component';
-import CreatePerson from './components/create-person.component';
-import DeletePerson from './components/delete-person.component';
+import ListPerson from "./components/list-person.component";
+import EditPerson from "./components/edit-person.component";
+import CreatePerson from "./components/create-person.component";
+import DeletePerson from "./components/delete-person.component";
+import Location from "./components/Location";
 
 const App = () => {
   return (
     <Router>
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <Link to="/" className="navbar-brand">Local need MERN App</Link>
+          <Link to="/" className="navbar-brand">
+            Local need MERN App
+          </Link>
           <div className="collapse navbar-collapse">
             <ul className="navbar-nav mr-auto">
               <li className="navbar-item">
-                <Link to="/" className="nav-link">List name of people in need</Link>
+                <Link to="/" className="nav-link">
+                  List name of people in need
+                </Link>
               </li>
               <li className="navbar-item">
-                <Link to="/create" className="nav-link"> Name</Link>
+                <Link to="/create" className="nav-link">
+                  {" "}
+                  Name
+                </Link>
               </li>
             </ul>
           </div>
@@ -30,9 +38,10 @@ const App = () => {
         <Route path="/edit/:id" component={EditPerson} />
         <Route path="/create" component={CreatePerson} />
         <Route path="/delete/:id" component={DeletePerson} />
+        <Location />
       </div>
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/location.js
+++ src/components/location.js
@@ -9,20 +9,28 @@ class Location extends React.Component {
       userLocation: null,
     };
 
-    // bind user location
+    // bind user location and coordinates
     this.getLocation = this.getLocation.bind(this)
+    this.getUserCoordinates = this.getUserCoordinates.bind(this)
 
   }
 
-//  get geolocation if supported by user browser
+//  use geolocation if supported by user browser to get coordinates
 getLocation() {
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getUserCoordinates);
+    navigator.geolocation.getCurrentPosition(this.getUserCoordinates);
 } else {
     alert("Geolocation is not supported by this browser!")
 }
 }
 
+getUserCoordinates(position){
+    console.log(position)
+    this.setState ({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    })
+}
 
   render() {
     return (
@@ -30,8 +38,8 @@ if (navigator.geolocation) {
         <h2>Distance</h2>
         <button onClick={this.getLocation}>Get Location</button>
         <h4>Cordinates</h4>
-        <p>Latitude</p>
-        <p>longitude</p>
+        <p>Latitude: {this.state.latitude}</p>
+        <p>longitude: {this.state.longitude}</p>
         <h4>Map Location</h4>
         <p>Address: {this.state.userLocation}</p>
       </div>
