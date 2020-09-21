3a7f5b3a5c40c544eaf3fffacdff0a051a244c6f
Carolene Wilson <carolenesw@gmail.com>
2020-09-21 10:23:57 -0400

    hnadle location error

--- src/App.js
+++ src/App.js
@@ -1,47 +1,71 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import ListPerson from "./components/list-person.component";
-import EditPerson from "./components/edit-person.component";
-import CreatePerson from "./components/create-person.component";
-import DeletePerson from "./components/delete-person.component";
-import Location from "./components/Location";
+class Location extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      longitude: null,
+      userLocation: null,
+    };
 
-const App = () => {
-  return (
-    <Router>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <Link to="/" className="navbar-brand">
-            Local need MERN App
-          </Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-                <Link to="/" className="nav-link">
-                  List name of people in need
-                </Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/create" className="nav-link">
-                  {" "}
-                  Name
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <br />
-        <Route path="/" exact component={ListPerson} />
-        <Route path="/edit/:id" component={EditPerson} />
-        <Route path="/create" component={CreatePerson} />
-        <Route path="/delete/:id" component={DeletePerson} />
-        <Location />
+    // bind user location and coordinates
+    this.getLocation = this.getLocation.bind(this)
+    this.getUserCoordinates = this.getUserCoordinates.bind(this)
+
+  }
+
+//  use geolocation if supported by user browser to get coordinates
+getLocation() {
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(this.getUserCoordinates, this.handleLocationErrors);
+} else {
+    alert("Geolocation is not supported by this browser!")
+}
+}
+
+getUserCoordinates(position){
+    console.log(position)
+    this.setState ({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    })
+}
+
+// handle location errors 
+handleLocationErrors(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+          alert( "User denied the request for Geolocation.")
+          break;
+        case error.POSITION_UNAVAILABLE:
+          alert( "Location information is unavailable.")
+          break;
+        case error.TIMEOUT:
+          alert( "The request to get user location timed out.")
+          break;
+        case error.UNKNOWN_ERROR:
+          alert( "An unknown error occurred.")
+          break;
+          default:
+          alert("An unknown error occurred.")
+      }
+    }
+
+
+  render() {
+    return (
+      <div className="location">
+        <h2>Distance</h2>
+        <button onClick={this.getLocation}>Get Location</button>
+        <h4>Cordinates</h4>
+        <p>Latitude: {this.state.latitude}</p>
+        <p>longitude: {this.state.longitude}</p>
+        <h4>Map Location</h4>
+        <p>Address: {this.state.userLocation}</p>
       </div>
-    </Router>
-  );
-};
+    );
+  }
+}
 
-export default App;
+export default Location;
