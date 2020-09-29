f84c53be697f279307e4c399d1d4a2c627eafce0
kellymedia <mk@uconn.edu>
2020-09-28 20:36:08 -0400

    update nav for mobile, update form and route css

--- src/components/editPerson.component.jsx
+++ src/components/editPerson.component.jsx
@@ -38,10 +38,10 @@ const EditPerson = (props) => {
     }
 
     return (
-        <div style={{}}>
+        <div className="edit" style={{}}>
             <div className="edit-img"></div>
             <h3><AiOutlineUserAdd /> Edit Name</h3>
-            <Form onSubmit={onSubmitPersonData}>
+            <Form className="edit-form" onSubmit={onSubmitPersonData}>
                 <FormGroup row>
                     <Col>
                         <Label><AiOutlineUser />  Name </Label>
